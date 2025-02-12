cf682e363ac021771e988ef0a2a3372b5dc19ce0
Andre Derjagin <derjagin.andre@gmail.com>
2025-02-12 20:45:50 +0100

    fixed deploy issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "https://exziil.github.io/hide-and-seek/",
+	base: "/hide-and-seek/",
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
