41d758546410947e33a2eb022c9c25460707e92e
Andre Derjagin <derjagin.andre@gmail.com>
2025-02-12 20:26:41 +0100

    add current location

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { LocationInitializer } from "./components/LocationInitializer";
 import Map from "./components/Map";
+import { useLocation } from "./stores/location";
 
 const queryClient = new QueryClient();
 
 function App() {
+	const location = useLocation();
+
 	return (
 		<QueryClientProvider client={queryClient}>
 			<div className="p-4">
 				<Map />
+
+				<div>
+					your current possition is:
+					<div>Lat: {location[0]}</div> <div>Lng: {location[1]}</div>
+				</div>
 			</div>
 			<LocationInitializer />
 		</QueryClientProvider>
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,13 +1,11 @@
 import "leaflet/dist/leaflet.css";
-import { MapContainer } from "react-leaflet";
+import { Circle, MapContainer } from "react-leaflet";
 import { TileLayer } from "react-leaflet/TileLayer";
 import { useLocation } from "../stores/location";
 
 function Map() {
 	// const POSITION: LatLngTuple = [49.007043030126354, 12.099156534308015];
 	const location = useLocation();
-
-	console.log("location: ", location);
 
 	return (
 		<MapContainer
@@ -18,6 +16,11 @@ function Map() {
 			<TileLayer
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+			/>
+
+			<Circle
+				center={location}
+				radius={6}
 			/>
 		</MapContainer>
 	);
