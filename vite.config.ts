41ea69232fd3faa84a48f9c99323570282cf2946
Andre Derjagin <derjagin.andre@gmail.com>
2025-02-12 20:57:26 +0100

    fixed deploy?

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/hide-and-seek/",
+	base: "./",
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
